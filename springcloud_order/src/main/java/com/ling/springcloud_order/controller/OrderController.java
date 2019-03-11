4bf1e91ca0e324f7ea24c744bd27ffdb42d09e77
lingzi <17778029272@163.com>
2019-03-11 21:24:13 +0800

    刷新自定义配置文件，不加，它只能刷新框架自己的

--- springcloud_order/src/main/java/com/ling/springcloud_order/controller/OrderController.java
+++ springcloud_order/src/main/java/com/ling/springcloud_order/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.ling.springcloud_order.entity.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping("order")
+@RefreshScope //更新自定义配置文件
 public class OrderController {
 
 
