4f3d8a58530f96a3b799fa55bbe3d098e3cb8908
lingzi <17778029272@163.com>
2019-03-12 15:31:27 +0800

    改名

--- springcloud-config/src/test/java/com/ling/springclouconfig/SpringclouCondfigApplicationTests.java
+++ springcloud-config/src/test/java/com/ling/springclouconfig/SpringclouCondfigApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringclouConfigApplicationTests {
+public class SpringclouCondfigApplicationTests {
 
     @Test
     public void contextLoads() {
